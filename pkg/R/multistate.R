604cb013d142d386f5625d8cdd7d2432afc196d4
hreulen (Holger Reulen) <hreulen@uni-goettingen.de>
2013-06-13 14:20:15 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gamboostmsm@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/multistate.R
+++ pkg/R/multistate.R
@@ -13,34 +13,35 @@ multistate <- function(numerator = numerator, denominator = denominator,
       zi.list[[hi]] <- .cpp_helpfun_z_i(etahat = f, 
                                         numerator = as.matrix(numerator.list[[hi]]), 
                                         denominator = denominator.list[[hi]])}
-    rm(hi); rm(K)
     zi <- do.call(cbind, zi.list)
-    rm(zi.list)
     zi <- apply(zi, MARGIN=1, FUN=sum)
-    zi <- 1-zi
-    zi <- zi - mean(zi)
+    event <- as.numeric(!is.na(hi.for.log.pl))    
+    zi <- event - zi
     return(zi)}
-  #########################################################################
-  ## loss a.k.a. negative log likelihood of a cox type multi state model ##
-  #########################################################################
-  multistate.loss <- function(y, f, w=NULL){
+  #############################################################
+  ## negative log likelihood of a cox type multi state model ##
+  #############################################################
+  multistate.nlpl <- function(y, f, w=NULL){
     numerator.list <- numerator
-    hi.for.log.pl <- hi.for.log.pl
     n <- length(hi.for.log.pl)
     K <- length(numerator.list)
-    log.pl.list <- vector("list", K)
+    lpl.list <- vector("list", K)
     for(hi in 1:K){
-      log.pl.list[[hi]] <- .cpp_helpfun_log_pl(etahat = f, 
-                                               numerator = numerator.list[[hi]])}
-    log.pl.now <- NULL
-    for(j in 1:n){
-      log.pl.now <- c(log.pl.now, log.pl.list[[hi.for.log.pl[j]]][j])}
-    return(-(log.pl.now))}
-  #########################################
-  ## construct new family using Family() ##
-  #########################################
+      lpl.list[[hi]] <- .cpp_helpfun_log_pl(etahat = f, 
+                                            numerator = numerator.list[[hi]])}
+    lpl <- rep(0, n)
+    event <- !is.na(hi.for.log.pl)
+    for(hi in 1:n){
+      if(event[hi]){
+        lpl[hi] <- lpl.list[[hi.for.log.pl[hi]]][hi]
+        }
+      }
+    return(-1*lpl)}
+  ###################################
+  ## construct new boosting family ##
+  ###################################
   Family(
     ngradient = multistate.ngradient,
-    loss = multistate.loss,
+    risk = function(y, f, w) sum(multistate.nlpl(y, f, w)),
     offset = function(y, w) 0,
-    name = "family for multi state models.")}
+    name = "Family for multi state models.")}
--- pkg/src/cpp_helpfun_z_i.cpp
+++ pkg/src/cpp_helpfun_z_i.cpp
@@ -4,18 +4,18 @@ using namespace Rcpp;
 NumericVector cpp_helpfun_z_i(NumericVector etahat, NumericMatrix numerator, List denominator){
   NumericVector eta(etahat);
   NumericVector expeta = exp(eta);
-  NumericMatrix indikatormatrix(numerator);
+  NumericMatrix indicatormatrix(numerator);
   List denominatorlistouter(denominator);
   int n = eta.size();
   NumericVector zi(n);
   NumericMatrix denominatoreta(n,n);
   NumericMatrix numeratoreta(n,n);
   std::fill(numeratoreta.begin(), numeratoreta.end(), 0.0);
-  std::fill(denominatoreta.begin(), denominatoreta.end(), 0.0001);
+  std::fill(denominatoreta.begin(), denominatoreta.end(), 1.0);
   for(int i=0; i<n; i++){
     List denominatorlistinner = wrap(denominatorlistouter[i]);
     for(int j=0; j<n; j++){
-      numeratoreta(i,j) = expeta(i)*indikatormatrix(j,i);
+      numeratoreta(i,j) = expeta(i)*indicatormatrix(i,j);
       IntegerVector denominatorindex = denominatorlistinner[j];
       if((denominatorindex(0)-1)<n){
 	denominatoreta(i,j) = 0.0;
@@ -27,7 +27,7 @@ NumericVector cpp_helpfun_z_i(NumericVector etahat, NumericMatrix numerator, Lis
 	numeratoreta(i,j) = 0;
       }
     }
-    double ratio = sum(numeratoreta(i,_)/(denominatoreta(i,_)));
+    double ratio = sum(numeratoreta(i,_)/denominatoreta(i,_));
     zi(i) = ratio;
   }
   return(wrap(zi));
